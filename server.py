cd90f834c9ce7b86390b11a6489f3434a8074040
Balaji Ramasubramanian <balaji.ramasubramanian@gmail.com>
2019-07-07 22:49:56 -0700

    Page is visible now

--- server.py
+++ server.py
@@ -5,47 +5,20 @@ app.secret_key = 'darksecret'
 
 import random
 
+DATA = {
+    'farm':[10,20],
+    'cave':[5,10],
+    'house':[2,5],
+    'casino':[-50,50],
+}
+
 @app.route("/")
 def root():
-    if 'number' not in session:
-        session['number'] = random.randint(1,100)
-        session['message'] = 'start'
-        session['trials'] = 0
-        session['placeholder'] = random.randint(1,100)
-        session['oldguesses'] = list()
-        session['wasted']=False
-    print("inside root() before render_template()")
-    return render_template("game.html")
+    return render_template("game.html", DATA=DATA)
 
-@app.route("/guess", methods=['POST'])
-def guess():
-    print("inside guess() before everything")
-    session['trials'] += 1
-    try:
-        session['guess']=int(request.form['guess'])
-    except:
-        session['guess']=session['placeholder']
-    if session['guess'] in session['oldguesses']:
-        session['wasted']=True
-    else:
-        session['wasted']=False
-        session['oldguesses'].append(session['guess'])
-    if session['guess']==session['number']:
-        session['message'] = 'match'
-    else:
-        if session['guess'] > session['number']:
-            session['message'] = 'high'
-        else:
-            session['message'] = 'low'
-    session['placeholder'] = random.randint(1,100)
-    print("inside guess() before redirect()")
+@app.route("/process_money", methods=['POST'])
+def process_money():
     return redirect("/")
-
-@app.route("/restart")
-def restart():
-    session.clear()
-    return redirect("/")
-
 
 if __name__ == '__main__':
     app.run(debug=True)
